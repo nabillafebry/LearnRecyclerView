6a9a212d025fd259f77acb37a564492f900370ec
nabillafebry <nabilla.febry16@gmail.com>
2018-08-13 11:25:07 -0700

    Initial Commit

---
+++ app/src/main/java/com/feby/asyst/learnrecyclerview/adapter/AlbumAdapter.java
@@ -0,0 +1,73 @@
+package com.feby.asyst.learnrecyclerview.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.feby.asyst.learnrecyclerview.R;
+import com.feby.asyst.learnrecyclerview.model.Album;
+
+import java.util.ArrayList;
+
+public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.MyViewHolder>{
+
+    Context mContext;
+    ArrayList<Album> mListAlbum;
+
+    public AlbumAdapter(Context context, ArrayList<Album> listAlbum) {
+        this.mContext = context;
+        this.mListAlbum = listAlbum;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView ivAlbum;
+        TextView tvAlbum, tvArtist;
+        CardView cardView;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            ivAlbum = itemView.findViewById(R.id.album_imageview);
+            tvAlbum = itemView.findViewById(R.id.album_textview);
+            tvArtist = itemView.findViewById(R.id.artist_name_textview);
+
+            cardView = itemView.findViewById(R.id.cardview);
+
+
+        }
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View itemVI = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_album, parent, false);
+
+        return new AlbumAdapter.MyViewHolder(itemVI);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        Album album = mListAlbum.get(position);
+
+        holder.tvArtist.setText(album.getArtist());
+        holder.tvAlbum.setText(album.getTitle());
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mListAlbum.size();
+    }
+
+}
---
+++ app/src/main/java/com/feby/asyst/learnrecyclerview/model/Album.java
@@ -0,0 +1,30 @@
+package com.feby.asyst.learnrecyclerview.model;
+
+public class Album {
+
+    String title, artist, image;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
